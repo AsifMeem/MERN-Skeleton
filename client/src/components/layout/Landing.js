068829058b08679097d2cf2d572ba302fc7dd8be
AsifMeem <asif.foysalm@gmail.com>
2020-05-13 22:46:57 +1000

    Add dashboard redirect for logged in user

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-const Landing = () => {
-    return (
-        <section className="landing">
-            <div className="dark-overlay">
-                <div className="landing-inner">
-                <h1 className="x-large">Techforia Team Portal</h1>
-                <p className="lead">
-                    Create your Techforia Profile and much more!
-                </p>
-                <div className="buttons">
-                    <Link to="/register" className="btn btn-primary">
-                        Sign Up
-                    </Link>
-                    <Link to="/login" className="btn btn-light">
-                        Login
-                    </Link>
-                </div>
-                </div>
-            </div>
-        </section>
-    )
-}
-export default Landing
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Techforia Team Portal</h1>
+          <p className="lead">Create your Techforia Profile and much more!</p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-light">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(Landing);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,15 +1,21 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { logout } from '../../actions/auth';
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logout } from "../../actions/auth";
 
 const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user"></i>{" "}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={logout} href="#!">
-          <i className="fas fa-sign-out-alt"></i>{' '}
+          <i className="fas fa-sign-out-alt"></i>{" "}
           <span className="hide-sm">Logout</span>
         </a>
       </li>
