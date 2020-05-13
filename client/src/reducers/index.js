a037f0c228185162e39aa4972c8fb4476a2c5a71
AsifMeem <asif.foysalm@gmail.com>
2020-05-13 22:37:35 +1000

    Create profile reducer and connect to Dashboard component

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
-import { combineReducers } from 'redux';
-import alert from './alert';
-import auth from './auth';
+import { combineReducers } from "redux";
+import alert from "./alert";
+import auth from "./auth";
+import profile from "./profile";
 
 //This is root reducer file
 export default combineReducers({
   alert,
   auth,
+  profile,
 });
